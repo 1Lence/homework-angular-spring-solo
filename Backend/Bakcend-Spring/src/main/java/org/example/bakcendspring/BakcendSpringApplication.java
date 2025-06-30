b7c3d68fbf14a4969a468fe1ee3436c01fc46737
1Lence <kirillpetrov173@gmail.com>
2025-06-30 17:46:03 +0500

    Created Project

---
+++ Backend/Bakcend-Spring/src/main/java/org/example/bakcendspring/BakcendSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.bakcendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakcendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakcendSpringApplication.class, args);
+    }
+
+}
---
+++ Backend/Bakcend-Spring/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,3 @@
+--liquibase formatted sql
+
+--changeset petrov:1
