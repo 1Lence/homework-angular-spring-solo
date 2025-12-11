8129d978409003922d5344b64fef09c8ac192b38
Kirill Petrov <kirillpetrov173@gmail.com>
2025-12-11 16:28:29 +0500

    Обнова

--- Backend/Bakcend-Spring/src/main/java/org/example/bakcendspring/Config/CorsConfig.java
+++ Backend/Bakcend-Spring/src/main/java/org/example/bakcendspring/Config/CorsConfig.java
@@ -1,15 +1,19 @@
 package org.example.bakcendspring.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${app.front-url}")
+    private String frontUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200")
+                .allowedOriginPatterns(frontUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
